a4b24025d90fd5e7b4b2bd7c2e049c5459cb0d37
HernaniFilho <hernanipcf@gmail.com>
2025-04-21 14:55:36 -0300

    Criação de páginas para Visualizar/Adicionar/Deletar/Editar armas do personagem

---
+++ dnd-weapons/Controllers/WeaponController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dnd_weapons.Controllers
+{
+    public class WeaponController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewWeapon()
+        {
+            return View();
+        }
+
+        public IActionResult AddWeapon()
+        {
+            return View();
+        }
+
+        public IActionResult EditWeapon()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteWeapon()
+        {
+            return View();
+        }
+    }
+}
