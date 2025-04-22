fa0190644fb2fd75602e44062ad4a36f01fb6f39
HernaniFilho <hernanipcf@gmail.com>
2025-04-22 15:18:36 -0300

    Usando DataTables para Tabela responsiva e moderna

--- dnd-weapons/wwwroot/js/site.js
+++ dnd-weapons/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('#myTable').DataTable({
+    "responsive": true,
+    columnDefs: [
+        {
+            targets: 3,
+            className: 'none',
+            responsivePriority: 1 // Mostra esta coluna primeiro quando a tela for menor
+        },
+        {
+            targets: 4,
+            className: 'none',
+            responsivePriority: 2 // Mostra a coluna depois
+        }
+    ],
+    "ordering": true,
+    "paging": true,
+    "searching": true,
+    "oLanguage": {
+        "sEmptyTable": "Nenhum registro encontrado na tabela",
+        "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+        "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sSearch": "Pesquisar",
+        "oPaginate": {
+            "sNext": "Proximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Ultimo"
+        },
+        "oAria": {
+            "sSortAscending": ": Ordenar colunas de forma ascendente",
+            "sSortDescending": ": Ordenar colunas de forma descendente"
+        }
+    }
+});
