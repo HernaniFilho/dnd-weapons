c60f633b1b2a4adbdad01de7a2760f46c3818d36
HernaniFilho <hernanipcf@gmail.com>
2025-05-03 14:03:27 -0300

    Estilizando as páginas e preparando modelos para a relação muitos-para-muitos entre WeaponModel e MaterialModel, falta fazer a migration

---
+++ dnd-weapons/Models/WeaponMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dnd_weapons.Models
+{
+    public class WeaponMaterial
+    {
+        public int WeaponId { get; set; }
+        public int MaterialId { get; set; }
+        public WeaponModel? Weapon { get; set; }
+        public MaterialModel? Material { get; set; }
+        [Required(ErrorMessage = "Quantidade é obrigatória")]
+        public required int Quantity { get; set; } = 1; // Default quantity is 1
+    }
+}
